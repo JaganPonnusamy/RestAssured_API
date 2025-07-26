057335c88eb0fb51fd562877355ae65a37d519e6
jaganponnusamy <jaganponnusamy@localhost>
2025-07-26 13:38:46 +0530

    Initial Commit - Rest Assured

---
+++ src/test/java/RestAPI/Test_1.java
@@ -0,0 +1,20 @@
+package RestAPI;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class Test_1 {
+	
+	@Test
+	public void Test1()
+	{
+		Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+		response.getStatusCode();
+		response.getTime();
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getTime());
+		System.out.println(response.getBody().asPrettyString());
+	}
+
+}
